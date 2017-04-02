86d4c176b5c21363de5bd35c384f95ecf421f57a
derekbaker1986 <developer@arsuperior.com>
2017-04-02 16:01:30 -0400

    sigh

--- controllers/app.config.js
+++ controllers/app.config.js
@@ -51,7 +51,7 @@ app.config(function($routeProvider) {
         controller  : "mailCTRL"
     })
     .when("/accounts", {
-        templateUrl : "partials/accounts.html?v=1.1",
+        templateUrl : "partials/accounts.html?v=1.2",
         controller  : "accountsCTRL"
     })
     .when("/help", {
@@ -117,7 +117,7 @@ app.config(function($routeProvider) {
    })
    // NOTE: SSH PuTTY page is at the bottom
    .when("/email", {
-       templateUrl : "partials/help/services/email.html?v=1.1",
+       templateUrl : "partials/help/services/email.html?v=1.2",
        controller  : "helpCTRL"
    })
    .when("/mysql", {
